332c7d063ad3a658616cc962bd8959bc13933b62
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2011-05-05 02:13:48 -0400

    Add support for requiring utility gems not included in the Gemfile for a project

--- irb.d/02-load_gems.rb
+++ irb.d/02-load_gems.rb
@@ -1,16 +1,63 @@
 #!/usr/bin/env ruby
 # -*- ruby -*-
+module IRBExtensions
+  # Load a gem into IRB regardless of Bundler trickery. This method leaves
+  # $LOAD_PATH altered, in order to allow #autoload to work properly.
+  #
+  #   Inspired by:
+  #     https://gist.github.com/955611
+  #     https://gist.github.com/932231
+  #
+  # IRB.require_gem 'looksee'
+  # IRB.require_gem 'awesome_print', :as => 'ap'
+  def require_gem(name, options = {})
+    name = name.to_s
+    require_as = (options[:as] || options['as'] || name).to_s
 
-libs = %w(looksee wirble interactive_editor)
+    require require_as
+  rescue LoadError => e
+    gem_path = find_gem_path(name)
 
-if RUBY_VERSION < "1.9"
-  libs.unshift 'rubygems'
+    $LOAD_PATH.unshift(gem_path) unless gem_path.nil?
+    require require_as
+  end
+
+  private
+    # Searches for the path of the specified gem, returning the lib path to the
+    # most recent version of the gem, or nil if no matching path is found.
+    def find_gem_path(name)
+      candidates = []
+
+      Gem.path.each do |path|
+        glob = File.join(path, 'gems/*')
+
+        Dir.glob(glob) do |entry|
+          basename = File.basename(entry)
+          gemname, sep, version = basename.rpartition('-')
+
+          if gemname == name
+            libdir = File.expand_path(File.join(entry, 'lib'))
+            class << libdir; attr_accessor(:gemname); attr_accessor(:version); end
+            libdir.gemname = gemname
+            libdir.version = version.scan(/\d+/).map {|digit| digit.to_i}
+            candidates.push(libdir)
+          end
+        end
+      end
+
+      candidates.sort! {|a, b| a.version <=> b.version}
+      candidates.last
+    end
 end
+IRB.instance_eval { extend IRBExtensions } if defined?(IRB)
+
+libs = %w(wirble looksee interactive_editor)
+libs.unshift('rubygems')  if RUBY_VERSION < '1.9'
 
 libs.each do |lib|
   begin
-    require lib
-  rescue LoadError => e
+    IRB.require_gem lib
+  rescue Exception => e
     puts "Failed to load #{lib}: #{e.message}"
   end
 end
