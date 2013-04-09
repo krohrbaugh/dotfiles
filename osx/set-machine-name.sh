6e9c10fffaa7ab69f61ab801cd503cbdd6c0de9a
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2013-04-08 21:43:17 -0700

    Use variable for machine name

--- osx/set-machine-name.sh
+++ osx/set-machine-name.sh
@@ -10,7 +10,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Set computer name (as done via System Preferences → Sharing)
 # 	See: http://muppet.wikia.com/wiki/Sweetums
-sudo scutil --set ComputerName "Sweetums"
-sudo scutil --set HostName "Sweetums"
-sudo scutil --set LocalHostName "Sweetums"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "Sweetums"
+HOSTNAME="sweetums"
+sudo scutil --set ComputerName $HOSTNAME
+sudo scutil --set HostName $HOSTNAME
+sudo scutil --set LocalHostName $HOSTNAME
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $HOSTNAME
+
+unset HOSTNAME
