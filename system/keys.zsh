7332cf41a02a26555f0e508f6ec121eeb076e7f2
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2013-04-09 12:00:33 -0700

    Use id_rsa.pub for the pubkey alias

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,2 @@
 # Pipe my public key to clipboard
-alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied!'"
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied!'"
