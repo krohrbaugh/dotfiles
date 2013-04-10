cb143e01b5b88ce05e2ec509e6002ada1553ceb4
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2013-04-10 07:32:29 -0700

    Add ~/.bin to PATH for secret binaries/scripts

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:$HOME/.bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
