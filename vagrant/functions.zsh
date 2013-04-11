83bdd23a02eaf988a54b38a8e57e300f6cbb0f48
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2013-04-10 21:00:34 -0700

    Add vagrant UUID function for snapshot purposes

---
+++ vagrant/functions.zsh
@@ -0,0 +1,16 @@
+#
+# Allows for snapshots from the command line:
+#   vboxmanage snapshot $(vagrant-uuid) list
+#   vboxmanage snapshot $(vagrant-uuid) take <name>
+#   vboxmanage snapshot $(vagrant-uuid) restore <name>
+#   vboxmanage snapshot $(vagrant-uuid) delete <name>
+#
+# Note: Commands above must be run from the same location as .vagrant
+vagrant-uuid() {
+  if [ -f .vagrant ]
+  then
+    echo $(cat .vagrant | python -c 'import json,sys;obj=json.loads(sys.stdin.read());print obj["'"active"'"]["'"default"'"]')
+  else
+    echo "No UUID found"    
+  fi
+}
