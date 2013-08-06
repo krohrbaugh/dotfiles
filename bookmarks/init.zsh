4b9eedd4a72a86c9a9601373969cfe33f718ffc6
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2013-08-05 18:04:01 -0700

    Add directory bookmarks
    - Use ZSH command hash for storing directory bookmarks.
    - Git ignore hidden files in sub-directories, in order to allow for private bookmarks (e.g., bookmarks/.private)

---
+++ bookmarks/init.zsh
@@ -0,0 +1,41 @@
+#
+# Directory bookmarks
+#   Adds named directory to ZSH command hash, if the directory exists.
+#
+# Use:
+#   1. Define bookmarks in bookmark files (one bookmark per line)
+#       # ./bookmarks/personal
+#       name=path/to/bookmark
+#   2. Use bookmarks in shell:
+#       $ ~name # => cd path/to/bookmark
+#   3. There is no step 3
+#
+setopt autocd
+
+typeset -A dir_bookmarks
+dir_bookmarks=()
+
+typeset -U bookmark_files
+bookmark_files=($ZSH/bookmarks/(.*|*))
+
+# Load bookmarks, excluding this init script
+for file in ${${bookmark_files:#*/init.zsh}}
+do
+  for line in `cat $file`
+  do
+    bookmark=(${(s/=/)line})
+    dir_bookmarks[$bookmark[1]]=${(e)bookmark[2]}
+  done
+done
+
+for bookmark in ${(k)dir_bookmarks}
+do
+  bookmark_path=$dir_bookmarks[$bookmark]
+  if [[ -d "$bookmark_path" ]]
+  then
+    hash -d $bookmark=$bookmark_path
+  fi
+done
+
+unset bookmark_files
+unset dir_bookmarks
