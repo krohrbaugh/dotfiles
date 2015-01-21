5df26b15cff9f4cf4b7acc8fe672ce2a0bc98fbd
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2015-01-21 10:06:56 -0800

    Update DNS cache clear alias

--- system/network.zsh
+++ system/network.zsh
@@ -1,1 +1,11 @@
-alias flushdns="sudo killall -HUP mDNSResponder"
+# OS X 10.10
+alias flushdns="sudo discoveryutil udnsflushcaches"
+
+# MacOS X 10.9
+# alias flushdns="dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+
+# MacOS X 10.7 - 10.8
+# alias flushdns="sudo killall -HUP mDNSResponder"
+
+# MacOS X 10.5 - 10.6
+# alias flushdns="sudo dscacheutil -flushcache"
