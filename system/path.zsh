c538c9885aa93186e4f27d052c4dc843029abde8
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2016-03-31 20:44:37 -0700

    Support zprofile

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/bin/npm:$ZSH/bin:$HOME/.bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
