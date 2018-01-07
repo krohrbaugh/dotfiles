fff0201947b5208e469870f128e8c41ada0e2d4b
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2018-01-07 12:06:00 -0800

    Default to existing git config when present

--- script/shared/generate_gitconfig.sh
+++ script/shared/generate_gitconfig.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 #
-# bootstrap installs things.
+# Generates gitconfig.symlink using gitconfig.template
 source "${DOTFILES_ROOT}/script/shared/common.sh"
 
 generate_gitconfig () {
-  if ! [ -f git/gitconfig.symlink ]
-  then
+  if ! [ -f git/gitconfig.symlink ]; then
     info 'setup gitconfig'
 
-    user ' - What is your github author name?'
-    read -e git_authorname
-    user ' - What is your github author email?'
-    read -e git_authoremail
+    # Default to existing configuration when present
+    if git config user.name &>/dev/null; then
+      GIT_NAME="$(git config user.name)"
+    else
+      user ' - What is your git display name?'
+      read -e GIT_NAME
+    fi
+    info "GIT NAME: $GIT_NAME"
 
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.template > git/gitconfig.symlink
+    if git config user.name &>/dev/null; then
+      GIT_EMAIL="$(git config user.email)"
+    else
+      user ' - What is your git email?'
+      read -e GIT_EMAIL
+    fi
+    
+    if git config github.user &>/dev/null; then
+      GITHUB_USER="$(git config github.user)"
+    else
+      user ' - What is your Github username?'
+      read -e GITHUB_USER
+    fi
+
+    # Generate gitconfig from template
+    sed -e "s/GIT_NAME/$GIT_NAME/g" \
+        -e "s/GIT_EMAIL/$GIT_EMAIL/g" \
+        -e "s/GITHUB_USER/$GITHUB_USER/g" \
+        git/gitconfig.template > git/gitconfig.symlink
 
     success 'generated gitconfig'
   else
