61b744a08a999ed1f33ce5acd4a995881fada793
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2018-04-02 16:09:19 -0400

    Remove rbenv from path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOT_FILES/bin:$HOME/.bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
