4dbbeeeef3467753caaf950cdb106a3bce962e42
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2018-04-02 22:41:36 -0400

    Only setup Sublime Text 3 preferences once

--- sublime3/sublime3.sh
+++ sublime3/sublime3.sh
@@ -7,29 +7,20 @@ source "$DOT_FILES/script/shared/paths.sh"
 # shellcheck source=script/shared/common.sh
 source "$DOT_FILES/script/shared/common.sh"
 
+sl3_user_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+sl3_backup_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User.bak"
+sl3_dotfiles_dir="$DOT_FILES/sublime3/User"
+
 sublime3 () {
-  _sublime3_declarations
-  move_directory "$sl3_user_dir" "$sl3_backup_dir"
-  link_directory "$sl3_dotfiles_dir" "$sl3_user_dir"
-  _sublime3_unset
+  if ! [ -L "$sl3_user_dir" ]; then
+    move_directory "$sl3_user_dir" "$sl3_backup_dir"
+    link_directory "$sl3_dotfiles_dir" "$sl3_user_dir"
+  fi
 }
 
 sublime3_rm () {
-  _sublime3_declarations
-  remove_file "$sl3_user_dir"
-  move_directory "$sl3_backup_dir" "$sl3_user_dir"
-  _sublime3_unset
-}
-
-_sublime3_declarations () {
-  # Library/Application Support/Sublime Text 3/Packages/User
-  sl3_user_dir=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-  sl3_backup_dir=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User.bak
-  sl3_dotfiles_dir="$DOT_FILES/sublime3/User"
-}
-
-_sublime3_unset () {
-  unset sl3_user_dir
-  unset sl3_backup_dir
-  unset sl3_dotfiles_dir
+  if [ -L "$sl3_user_dir" ]; then
+    remove_file "$sl3_user_dir"
+    move_directory "$sl3_backup_dir" "$sl3_user_dir"
+  fi
 }
