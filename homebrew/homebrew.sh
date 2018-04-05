2544f2468cc9360f7087a87ce5a82477df83e322
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2018-04-04 22:25:12 -0700

    Refactor software installation to use setup/update/nuke scripts

---
+++ homebrew/homebrew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Functions for installing software
+
+# shellcheck source=script/shared/paths.sh
+source "$DOT_FILES/script/shared/paths.sh"
+# shellcheck source=script/shared/common.sh
+source "$DOT_FILES/script/shared/common.sh"
+
+homebrew_bundle () {
+  info 'checking Brewfile . . . '
+  brew bundle check --global || brew bundle --global
+}
