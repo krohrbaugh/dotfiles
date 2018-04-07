f3cba61e121f2cc9ac30473e8e842da0fb18294c
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2018-04-06 22:03:04 -0700

    Update simulator command for Xcode 9.3

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,5 +1,5 @@
 # Open iOS Simulator from command line
-alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
+alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 
 # Clear Xcode's DerivedData cache
 alias xcode-ded="rm -rf ~/Library/Developer/Xcode/DerivedData"
