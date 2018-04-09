f6f8e92225ba7aa20b58bd034ac700a184a92a11
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2018-04-08 20:56:26 -0700

    Cleanup node

--- node/node.sh
+++ node/node.sh
@@ -7,13 +7,6 @@ source "$DOT_FILES/script/shared/paths.sh"
 # shellcheck source=script/shared/common.sh
 source "$DOT_FILES/script/shared/common.sh"
 
-node_environment_bootstrap () {
-  info 'Checking Brewfile . . . '
-  # Install nvm via Homebrew
-  local node_brewfile="$DOT_FILES/node/Brewfile"
-  brew bundle check --file="$node_brewfile" || brew bundle --file="$node_brewfile"
-}
-
 node_environment () {
   make_directory "$NVM_DIR"
 }
