f4e24035b148981928062d7be0d4719454008657
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2018-10-22 21:30:44 -0700

    VSCode settings

---
+++ vscode/vscode.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Functions for configuring VS Code
+
+# shellcheck source=script/shared/paths.sh
+source "$DOT_FILES/script/shared/paths.sh"
+# shellcheck source=script/shared/common.sh
+source "$DOT_FILES/script/shared/common.sh"
+
+vscode_user_dir="$HOME/Library/Application Support/Code/User"
+vscode_backup_dir="$HOME/Library/Application Support/Code/User.bak"
+vscode_dotfiles_dir="$DOT_FILES/vscode/User"
+
+vscode () {
+  if ! [ -L "$vscode_user_dir" ]; then
+    move_directory "$vscode_user_dir" "$vscode_backup_dir"
+    link_directory "$vscode_dotfiles_dir" "$vscode_user_dir"
+  fi
+}
+
+vscode_rm () {
+  if [ -L "$vscode_backup_dir" ]; then
+    remove_file "$vscode_user_dir"
+    move_directory "$vscode_backup_dir" "$vscode_user_dir"
+  fi
+}
