1566917dd7bab2ac2817d4f12744f3d6d30b0128
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2019-04-21 16:58:06 -0700

    Use scripts-to-rule-them-all consistently

--- homebrew/homebrew.sh
+++ homebrew/homebrew.sh
@@ -8,6 +8,19 @@ source "$DOT_FILES/script/shared/paths.sh"
 source "$DOT_FILES/script/shared/common.sh"
 
 homebrew_bundle () {
+  brew update
   info 'Checking Brewfile . . . '
   brew bundle check --global || brew bundle --global
+}
+
+homebrew_update () {
+  brew update
+}
+
+homebrew_upgrade () {
+  brew upgrade
+}
+
+homebrew_info () {
+  brew info
 }
