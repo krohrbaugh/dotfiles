c15cd02a9f5fd05180bb25a06580d1991691cae3
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2019-05-21 15:10:06 -0700

    - Use consistent script naming nomenclature for terminal - Change default shell command so it works; fixes GH-2 - Run Terminal and Sublime3 setup from script/setup

--- terminal/terminal.sh
+++ terminal/terminal.sh
@@ -13,15 +13,17 @@ shell_preference () {
   local -r preferred_shell="$1"
 
   if [ "$preferred_shell" != "$SHELL" ] && [ -x "$preferred_shell" ]; then
-    "sudo dscl . -create /Users/$USER UserShell $preferred_shell"
+    echo "Trying to set default shell to $preferred_shell"
+    sudo dscl localhost -create /Local/Default/Users/"$USER" UserShell "$preferred_shell"
 	  successn "set default shell to $preferred_shell"
+  else
+    warnn "skipped changing default shell to $preferred_shell: already set, or does not exist"
   fi
 }
 
 shell_zsh () {
   # Set user shell to Homebrew-installed ZSH
   shell_preference "/usr/local/bin/zsh"
-  make_directory ~/.zsh
 }
 
 shell_bash () {
@@ -29,18 +31,20 @@ shell_bash () {
   shell_preference "/bin/bash"
 }
 
-terminal_preferences () {
+terminal_environment () {
   # Set terminal configuration. They can only be set when _not_ running the
   # terminal app, so this first detects which app is running, and sets config
   # for the _other_ one
   if [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
     plist_preferences com.googlecode.iterm2
+    successn "updated preferences for iTerm; to update Terminal preferences, run again from iTerm! O_o"
   else
     plist_preferences com.apple.Terminal
+    successn "updated preferences for Terminal; to update iTerm preferences, run again from Terminal! O_o"
   fi
 }
 
-terminal_preferences_rm () {
+terminal_environment_rm () {
   # Remove terminal configuration.
   plist_preferences_rm com.googlecode.iterm2
   plist_preferences_rm com.apple.Terminal
