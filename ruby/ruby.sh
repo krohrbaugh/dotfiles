050267783d15c8c1e756cf1bc16b7bc95777ab93
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2019-05-21 14:17:47 -0700

    Use consistent shell naming structure

--- ruby/ruby.sh
+++ ruby/ruby.sh
@@ -7,11 +7,19 @@ source "$DOT_FILES/script/shared/paths.sh"
 # shellcheck source=script/shared/common.sh
 source "$DOT_FILES/script/shared/common.sh"
 
+if [ -z ${RUBIES_DIR+x} ]; then
+  RUBIES_DIR="$HOME/.rubies"
+fi
+
 ruby_environment () {
-  # Install latest stable MRI Ruby
-  ruby-install ruby --no-reinstall
+  make_directory "$RUBIES_DIR"
 }
 
 ruby_environment_rm () {
-  remove_directory "$HOME/.rubies"
+  remove_directory "$RUBIES_DIR"
+}
+
+ruby_update () {
+  # Install latest stable MRI Ruby
+  ruby-install --latest --no-reinstall ruby
 }
