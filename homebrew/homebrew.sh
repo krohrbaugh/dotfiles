7b6aa09c7737abc64856cdb2a292527cf7521145
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2019-10-18 22:45:57 -0700

    Support excluding formulas in the Brewfile

--- homebrew/homebrew.sh
+++ homebrew/homebrew.sh
@@ -7,6 +7,24 @@ source "$DOT_FILES/script/shared/paths.sh"
 # shellcheck source=script/shared/common.sh
 source "$DOT_FILES/script/shared/common.sh"
 
+# Exclude functionality (see README)
+homebrew_exclude () {
+  if [ -f "$1" ]; then
+    paste -sd ' ' "$1"
+  else
+    echo "$1 does not exist"
+  fi
+}
+
+HOMEBREW_BUNDLE_BREW_SKIP=$(homebrew_exclude "$HOME/.homebrew_bundle_brew_exclude")
+HOMEBREW_BUNDLE_CASK_SKIP=$(homebrew_exclude "$HOME/.homebrew_bundle_cask_exclude")
+HOMEBREW_BUNDLE_MAS_SKIP=$(homebrew_exclude "$HOME/.homebrew_bundle_mas_exclude")
+HOMEBREW_BUNDLE_TAP_SKIP=$(homebrew_exclude "$HOME/.homebrew_bundle_tap_exclude")
+export HOMEBREW_BUNDLE_BREW_SKIP
+export HOMEBREW_BUNDLE_CASK_SKIP
+export HOMEBREW_BUNDLE_MAS_SKIP
+export HOMEBREW_BUNDLE_TAP_SKIP
+
 homebrew_bundle () {
   brew update
   info 'Checking Brewfile . . . '
