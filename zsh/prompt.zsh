230f54c49a64a2338e2ad300603cde034fcb79f5
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2019-10-19 23:12:46 -0700

    WIP: Refactor git prompt to be configurable

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # Prompt configuration
 #
@@ -6,42 +7,60 @@ autoload -Uz colors; colors
 
 if (( $+commands[git] ))
 then
-  git="$commands[git]"
+  git_cmd="$commands[git]"
 else
-  git="/usr/bin/git"
+  git_cmd="/usr/bin/git"
 fi
 
-git_branch() {
-  echo $($git rev-parse --abbrev-ref HEAD)
-}
-
-git_dirty() {
-  st=$($git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
-  else
-    if [[ "$st" =~ ^nothing ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
+git_status () {
+  # TODO: Resume here: need to determine whether to show dirty/unpushed based on config
+  # and short-circuit before the call to git status
+  if [ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
+    echo " on $(git_dirty)$(git_branch)%{$reset_color%}$(need_push)"
   fi
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
- echo "${ref#refs/heads/}"
+git_branch() {
+  echo $(git rev-parse --abbrev-ref HEAD)
 }
 
+git_dirty () {
+  git_status=$(git status --porcelain)
+  if [ -z "$git_status" ]; then
+    # Clean
+    echo "%{$fg_bold[green]%}"
+  else
+    # Dirty
+    echo "%{$fg_bold[red]%}"
+  fi
+}
+
+# git_dirty() {
+#   st=$(git status 2>/dev/null | tail -n 1)
+#   if [[ $st == "" ]]
+#   then
+#     echo ""
+#   else
+#     if [[ "$st" =~ ^nothing ]]
+#     then
+#       echo "on %{$fg_bold[green]%}$(git_branch)%{$reset_color%}"
+#     else
+#       echo "on %{$fg_bold[red]%}$(git_branch)%{$reset_color%}"
+#     fi
+#   fi
+# }
+
+# git_prompt_info () {
+#  ref=$($git_cmd symbolic-ref HEAD 2>/dev/null) || return
+#  echo "${ref#refs/heads/}"
+# }
+
 unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
+  $(git cherry -v @{upstream} 2>/dev/null)
 }
 
 need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
+  if [ -z "$(unpushed)" ]; then
     echo ""
   else
     echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
@@ -75,9 +94,12 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%{$reset_color%}"
 }
 
-export PROMPT=$'$(user_info) in $(directory_name) $(git_dirty)$(need_push)\n› '
+# export PROMPT=$'$(user_info) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'$(user_info) in $(directory_name)$(git_status)\n› '
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
   print
 }
+
+unset git_cmd
