6218cddcddae6db4e53f0e228d39ad259262298d
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2019-10-20 18:36:10 -0700

    Support prompt configuration and simplify colors

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,101 +5,93 @@
 # NOTE: Requires colors built-in to be loaded.
 autoload -Uz colors; colors
 
-if (( $+commands[git] ))
-then
-  git_cmd="$commands[git]"
-else
-  git_cmd="/usr/bin/git"
-fi
+prompt () (
+  user_info () {
+    echo "${FG[002]}%n@%m${FX[reset]}"
+  }
 
-git_status () {
-  # TODO: Resume here: need to determine whether to show dirty/unpushed based on config
-  # and short-circuit before the call to git status
-  if [ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
-    echo " on $(git_dirty_color)$(git_branch)%{$reset_color%}$(need_push)"
-  fi
-}
+  git_info () (
+    cmd_current_branch () {
+      git rev-parse --abbrev-ref HEAD
+    }
 
-git_branch () {
-  git rev-parse --abbrev-ref HEAD
-}
+    cmd_is_git_repo () {
+      command -v git > /dev/null 2>&1 &&
+        git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    }
 
-git_dirty_color () {
-  git_status=$(git status --porcelain)
-  if [ -z "$git_status" ]; then
-    # Clean
-    echo "%{$fg_bold[green]%}"
-  else
-    # Dirty
-    echo "%{$fg_bold[red]%}"
-  fi
-}
+    cmd_read_config () {
+      config_type="$1"
+      config_default="$2"
+      config_setting="$3"
+      git config --get --default "$config_default" --type "$config_type" \
+        "$config_setting"
+    }
 
-# git_dirty() {
-#   st=$(git status 2>/dev/null | tail -n 1)
-#   if [[ $st == "" ]]
-#   then
-#     echo ""
-#   else
-#     if [[ "$st" =~ ^nothing ]]
-#     then
-#       echo "on %{$fg_bold[green]%}$(git_branch)%{$reset_color%}"
-#     else
-#       echo "on %{$fg_bold[red]%}$(git_branch)%{$reset_color%}"
-#     fi
-#   fi
-# }
+    cmd_status () {
+      git status --porcelain
+    }
 
-# git_prompt_info () {
-#  ref=$($git_cmd symbolic-ref HEAD 2>/dev/null) || return
-#  echo "${ref#refs/heads/}"
-# }
+    cmd_unpushed_commits () {
+      git cherry -v @{upstream} 2>/dev/null
+    }
 
-unpushed () {
-  git cherry -v @{upstream} 2>/dev/null
-}
+    dirty_file_count () {
+      printf "%i" "$(cmd_status | wc -l)"
+    }
 
-need_push () {
-  if [ -z "$(unpushed)" ]; then
-    echo ""
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
+    dirty_color () {
+      # Default to unknown
+      dirty_color="${FG[135]}"
+      should_show_dirty=$(cmd_read_config "bool" "true" "shell.showdirty")
+      if [ "$should_show_dirty" = "true" ]; then
+        git_status=$(cmd_status)
+        if [ "$(dirty_file_count)" -eq 0 ]; then
+          dirty_color="${FG[010]}"
+        else
+          dirty_color="${FG[009]}"
+        fi
+      fi
+      echo "$dirty_color"
+    }
 
-user_info() {
-  echo "%{$fg[green]%}%n@%m%{$reset_color%}"
-}
+    branch_info () {
+      echo "${FX[bold]}$(dirty_color)$(cmd_current_branch)${FX[reset]}"
+    }
 
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if (( $+commands[todo.sh] ))
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
+    unpushed_commit_count () {
+      printf "%i" "$(cmd_unpushed_commits | wc -l)"
+    }
+
+    push_info () {
+      should_show_push=$(cmd_read_config "bool" "true" "shell.showpush")
+      if [ "$should_show_push" = "true" ]; then
+        unpushed_commits=$(unpushed_commit_count)
+        if [ "$unpushed_commits" -gt 0 ]; then
+          echo " with ${FG[013]}$unpushed_commits unpushed${FX[reset]}"
+        fi
+      fi
+    }
+
+    if cmd_is_git_repo; then
+      echo " on $(branch_info)$(push_info)"
     fi
-  else
-    echo ""
-  fi
-}
+  )
 
-directory_name(){
-  echo "%{$fg_bold[cyan]%}%1/%{$reset_color%}"
-}
+  path_info () {
+    echo " in ${FG[012]}%1/${FX[reset]}"
+  }
 
-# export PROMPT=$'$(user_info) in $(directory_name) $(git_dirty)$(need_push)\n› '
-export PROMPT=$'$(user_info) in $(directory_name)$(git_status)\n› '
+  printf "%s\n%s" \
+    "$(user_info)$(path_info)$(git_info)" \
+    "${FG[063]}› ${FX[reset]}"
+)
+
+PROMPT=$'$(prompt)'
+export PROMPT
+
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
   print
 }
-
-unset git_cmd
