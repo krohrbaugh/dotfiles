ab1fd49a2366d7c333cf1969e7cea22b5534f90c
Kevin Rohrbaugh <kevin@rohrbaugh.us>
2019-10-21 21:07:50 -0700

    Cleanup

--- asdf/completion.zsh
+++ asdf/completion.zsh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 #
 # Source asdf completion, when present
-if command -v brew &>/dev/null; then
-  # NOTE: It's possible that this path changes in the future, but hard-coding
-  # is faster than embedding `brew --prefix asdf`. \o/
-  asdf_completion="/usr/local/opt/asdf/etc/bash_completion.d/asdf.bash"
+#
+# NOTE: It's possible that this path changes in the future, but hard-coding
+# is faster than embedding `brew --prefix asdf`. \o/
+asdf_completion="/usr/local/opt/asdf/etc/bash_completion.d/asdf.bash"
 
-  if [ -f "$asdf_completion" ]; then
-    # shellcheck source=/dev/null
-    source "$asdf_completion"
-  else
-    echo "WARN: asdf completion not loaded"
-  fi
-
-  unset asdf_completion
+if [ -f "$asdf_completion" ]; then
+  # shellcheck source=/dev/null
+  source "$asdf_completion"
+else
+  echo "WARN: asdf completion not loaded"
 fi
+
+unset asdf_completion
