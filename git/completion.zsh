3db00d33e4b913478b3d9de927f8e8192745c48e
Kevin Rohrbaugh <krohrbaugh@twitter.com>
2019-10-24 10:25:57 -0700

    No need to source git completion; already in fpath

--- git/completion.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-git_completion="$(brew --prefix)/share/zsh/site-functions/_git"
-
-# shellcheck source=/dev/null
-[ -f "$git_completion" ] && source "$git_completion"
